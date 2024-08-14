80c8c200d14eba283e6f2df5922324736b061521
camdentadhg <camdent@gmail.com>
2024-08-13 19:47:10 -0500

    basic problems solved

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -78,20 +78,46 @@ class BinarySearchTree {
    * return the node, if found; else undefined. Uses iteration. */
 
   find(val) {
-
+    if (this.root){
+      let currentNode = this.root;
+      while (currentNode) {
+        if (val === currentNode.val) return currentNode;
+        if (val > currentNode.val){
+          currentNode = currentNode.right;
+        }
+        else {
+          currentNode = currentNode.left;
+        }
+      }
+    }
   }
 
   /** findRecursively(val): search the tree for a node with value val.
    * return the node, if found; else undefined. Uses recursion. */
 
   findRecursively(val) {
-
+    const findHelper = (node) => {
+      if (node){
+        if (val === node.val) return node;
+        if (val > node.val) return findHelper(node.right);
+        else return findHelper(node.left);
+      }
+    }
+    if (this.root)
+      return findHelper(this.root);
   }
 
   /** dfsPreOrder(): Traverse the array using pre-order DFS.
    * Return an array of visited nodes. */
 
   dfsPreOrder() {
+    const traverse = (node, array=[]) => {
+      array.push(node.val);
+      if (node.left) array = traverse(node.left, array);
+      if (node.right) array = traverse(node.right, array);
+      return array;
+    }
+    return traverse(this.root);
 
   }
 
@@ -99,22 +125,48 @@ class BinarySearchTree {
    * Return an array of visited nodes. */
 
   dfsInOrder() {
-
+    const traverse = (node, array=[]) => {
+      if (node.left) array = traverse(node.left, array);
+      array.push(node.val);
+      if (node.right) array = traverse(node.right, array);
+      return array;
+    }
+    return traverse(this.root);
   }
 
   /** dfsPostOrder(): Traverse the array using post-order DFS.
    * Return an array of visited nodes. */
 
   dfsPostOrder() {
-
+    const traverse = (node, array=[]) => {
+      if (node.left) array = traverse(node.left, array);
+      if (node.right) array = traverse(node.right, array);
+      array.push(node.val);
+      return array;
+    }
+    return traverse(this.root);
   }
 
   /** bfs(): Traverse the array using BFS.
    * Return an array of visited nodes. */
 
   bfs() {
-
-  }
+    const resultsArray = [];
+    //use a queue to keep track of nodes to visit
+    const toVisitQueue = [this.root];
+    //while the queue has length
+    while (toVisitQueue.length){
+      //pick the first value from the queue
+      let current = toVisitQueue.shift();
+      //push the node's value to the results array
+      resultsArray.push(current.val);
+      //add the node's left node to the queue
+      if (current.left) toVisitQueue.push(current.left);
+      //add the node's right node to the queue 
+      if (current.right) toVisitQueue.push(current.right);
+    }
+    return resultsArray;
+}
 
   /** Further Study!
    * remove(val): Removes a node in the BST with the value val.
